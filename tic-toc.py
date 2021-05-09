fe07559bf1efec3b9b676dca588069220ceae24f
JadeSbh <jadework777@gmail.com>
2021-05-09 19:05:40 -0400

    second commit

--- tic-toc.py
+++ tic-toc.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def getX(list_game):
     while True:
         try:
@@ -40,10 +43,19 @@ def print_game(yourlist):
     print(yourlist[2])
 
 
+def check_win(A):
+    for i in range(len(A)):
+        if A[i][0] == A[i][1] == A[i][2] \
+                or A[0][i] == A[1][i] == A[2][i] \
+                or A[0][0] == A[1][1] == A[2][2] \
+                or A[0][2] == A[1][1] == A[2][0]:
+            return 1
+
+
 list_default = [[0, 0, 0],
                 [0, 0, 0],
                 [0, 0, 0]
-             ]
+                ]
 
 print("Hello to this simple version of tic-toc game :) ")
 print("player X plays first")
@@ -56,10 +68,46 @@ print("Player X, it's your turn now ")
 roundX = getX(roundO)
 print_game(roundX)
 print("Player O, it's your turn now :) ")
-roundO = getX(roundX)
+roundO = getO(roundX)
 print_game(roundO)
 print("Player X, it's your turn now :) ")
 roundX = getX(roundO)
 print_game(roundX)
+winner = check_win(roundX)
+if winner == 1:
+    print("The winner is player X")
 
-
+else:
+    print("Player O, it's your turn now :) ")
+    roundO = getO(roundX)
+    print_game(roundO)
+    winner = check_win(roundO)
+    if winner == 1:
+        print("The winner is player O")
+        sys.exit()
+    else:
+        print("Player X, it's your turn")
+        roundX = getX(roundO)
+        print_game(roundX)
+        winner = check_win(roundX)
+        if winner == 1:
+            print("The winner is player X")
+            sys.exit()
+        else:
+            print("Player O, it's your turn now :) ")
+            roundO = getO(roundX)
+            print_game(roundO)
+            winner = check_win(roundO)
+            if winner == 1:
+                print("The winner is player O")
+                sys.exit()
+            else:
+                print("Player X, it's your turn")
+                roundX = getX(roundO)
+                print_game(roundX)
+                winner = check_win(roundX)
+                if winner == 1:
+                    print("The winner is player X")
+                    sys.exit()
+                else:
+                    print("it is a tie")
