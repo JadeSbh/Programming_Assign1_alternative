e9f48b8a5c6f0966116cef3f90c75701ee0833b6
JadeSbh <jadework777@gmail.com>
2021-05-09 23:19:50 -0400

    some optional features added

--- tic-toc.py
+++ tic-toc.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 
 def getX(list_game):
@@ -7,7 +8,7 @@ def getX(list_game):
             row = int(input("Enter the row:  "))
             col = int(input("Enter the column:  "))
             if row in [0, 1, 2] and col in [0, 1, 2]:
-                if list_game[row][col] == 0:
+                if list_game[row][col] == " ":
                     list_game[row][col] = "X"
                     return list_game
                 else:
@@ -24,7 +25,7 @@ def getO(list_game):
             row = int(input("Enter the row:  "))
             col = int(input("Enter the column:  "))
             if row in [0, 1, 2] and col in [0, 1, 2]:
-                if list_game[row][col] == 0:
+                if list_game[row][col] == " ":
                     list_game[row][col] = "O"
                     return list_game
                 else:
@@ -33,6 +34,17 @@ def getO(list_game):
                 print("Not in range, try again")
         except ValueError:
             print("Not an integer. Try again")
+
+
+def AU_getO(list_game):
+    while True:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+        if list_game[row][col] == " ":
+            list_game[row][col] = "O"
+            return list_game
+        else:
+            print("opps, it seems that I cannot play here, I am not that smart yet!")
 
 
 def print_game(yourlist):
@@ -52,46 +64,41 @@ def check_win(A):
             return 1
 
 
-list_default = [[0, 0, 0],
-                [0, 0, 0],
-                [0, 0, 0]
+list_default = [[" ", " ", " "],
+                [" ", " ", " "],
+                [" ", " ", " "]
                 ]
-print("Hello to this simple version of tic-toc game :) ")
-print("player X plays first")
-roundX = getX(list_default)
-print_game(roundX)
-print("Player O, it's your turn now ")
-roundO = getO(roundX)
-print_game(roundO)
-print("Player X, it's your turn now ")
-roundX = getX(roundO)
-print_game(roundX)
-print("Player O, it's your turn now :) ")
-roundO = getO(roundX)
-print_game(roundO)
-print("Player X, it's your turn now :) ")
-roundX = getX(roundO)
-print_game(roundX)
-winner = check_win(roundX)
-if winner == 1:
-    print("The winner is player X")
-
-else:
-    print("Player O, it's your turn now :) ")
-    roundO = getO(roundX)
-    print_game(roundO)
-    winner = check_win(roundO)
-    if winner == 1:
-        print("The winner is player O")
-        sys.exit()
-    else:
-        print("Player X, it's your turn")
+menu = """
+Hello, welcome to this simple version of Tic-Toc game.
+Please, make your choice:
+1.Player X VS player O
+2.Player X VS AI 
+3.quit
+"""
+while True:
+    print(menu)
+    choice = int(input("Please, choose from the menu 1,2 or 0 to quit:  "))
+    if choice == 1:
+        print("YOU CHOSE PLAYER X VS PLAYER O ")
+        print("player X plays first")
+        roundX = getX(list_default)
+        print_game(roundX)
+        print("Player O, it's your turn now ")
+        roundO = getO(roundX)
+        print_game(roundO)
+        print("Player X, it's your turn now ")
+        roundX = getX(roundO)
+        print_game(roundX)
+        print("Player O, it's your turn now :) ")
+        roundO = getO(roundX)
+        print_game(roundO)
+        print("Player X, it's your turn now :) ")
         roundX = getX(roundO)
         print_game(roundX)
         winner = check_win(roundX)
         if winner == 1:
             print("The winner is player X")
-            sys.exit()
+
         else:
             print("Player O, it's your turn now :) ")
             roundO = getO(roundX)
@@ -109,6 +116,80 @@ else:
                     print("The winner is player X")
                     sys.exit()
                 else:
-                    print("it is a tie")
+                    print("Player O, it's your turn now :) ")
+                    roundO = getO(roundX)
+                    print_game(roundO)
+                    winner = check_win(roundO)
+                    if winner == 1:
+                        print("The winner is player O")
+                        sys.exit()
+                    else:
+                        print("Player X, it's your turn")
+                        roundX = getX(roundO)
+                        print_game(roundX)
+                        winner = check_win(roundX)
+                        if winner == 1:
+                            print("The winner is player X")
+                            sys.exit()
+                        else:
+                            print("it is a tie")
+    elif choice == 2:
+        print("you chose to play with the computer")
+        print("player X, you play first")
+        roundX = getX(list_default)
+        print_game(roundX)
+        print("AU , This is my play ")
+        roundO = AU_getO(roundX)
+        print_game(roundO)
+        print("Player X, it's your turn now ")
+        roundX = getX(roundO)
+        print_game(roundX)
+        print("AU , This is my play")
+        roundO = AU_getO(roundX)
+        print_game(roundO)
+        print("Player X, it's your turn now :) ")
+        roundX = getX(roundO)
+        print_game(roundX)
+        winner = check_win(roundX)
+        if winner == 1:
+            print("The winner is player X")
 
-
+        else:
+            print("AU , it's your turn now :) ")
+            roundO = AU_getO(roundX)
+            print_game(roundO)
+            winner = check_win(roundO)
+            if winner == 1:
+                print("The winner is player O")
+                sys.exit()
+            else:
+                print("Player X, it's your turn")
+                roundX = getX(roundO)
+                print_game(roundX)
+                winner = check_win(roundX)
+                if winner == 1:
+                    print("The winner is player X")
+                    sys.exit()
+                else:
+                    print("AU , This is my play: ")
+                    roundO = AU_getO(roundX)
+                    print_game(roundO)
+                    winner = check_win(roundO)
+                    if winner == 1:
+                        print("The winner is player O")
+                        sys.exit()
+                    else:
+                        print("Player X, it's your turn")
+                        roundX = getX(roundO)
+                        print_game(roundX)
+                        winner = check_win(roundX)
+                        if winner == 1:
+                            print("The winner is player X")
+                            sys.exit()
+                        else:
+                            print("it is a tie")
+    elif choice == 0:
+        print("BYe BYE !")
+        break
+    else:
+        print("Please, choose from the menu 1, 2, or 0 to quit")
